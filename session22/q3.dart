0aebe5ccc9bf21daf6cdc105a5bf209b5a75e882
Diyabos <deabosman@gmail.com>
2026-01-24 21:32:34 +0200

    Add files via upload

---
+++ session22/q3.dart
@@ -0,0 +1,14 @@
+/**
+  Exercise 3:
+ a) Create String phrase = 'Learning Dart'.
+ b) Print phrase.length, phrase.toLowerCase(), and phrase.contains('Dart').
+ c) Trim spaces from String test = ' Dart ' and print the result
+  */
+void main() {
+  String languge = 'Learning Dart';
+  print(languge.length);
+  print(languge.toLowerCase());
+  print(languge.contains('Dart'));
+  String test = ' Dart ';
+  print(test.trim());
+}
