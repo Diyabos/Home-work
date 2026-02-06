1931a0b257d72a03c81e9117a050a4c49f3e758b
Diyabos <deabosman@gmail.com>
2026-02-06 22:25:29 +0200

    Add files via upload

---
+++ leetcode/case2.dart
@@ -0,0 +1,15 @@
+/**
+ Given an integer array nums, return true if any value appears
+ at least twice in the array, and return false if every element
+ is distinct
+ */
+bool containsDuplicate(List<int> nums) {
+  Set<int> seen = {};
+  for (int num in nums) {
+    if (seen.contains(num)) {
+      return true;
+    }
+    seen.add(num);
+  }
+  return false;
+}
