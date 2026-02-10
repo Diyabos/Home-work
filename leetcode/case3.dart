f13c1f12f52d0f4421f9264d1ffad9be6e597c6b
Diyabos <deabosman@gmail.com>
2026-02-10 15:00:07 +0200

    Add files via upload

---
+++ leetcode/case3.dart
@@ -0,0 +1,31 @@
+import 'dart:vmservice_io';
+
+/**
+ Best Time to Buy and Sell Stock
+ You are given an array prices where prices[i] is the price of 
+ a given stock on the ith day. You want to maximize your profit
+ by choosing a single day to buy one stock and choosing a
+  different day in the future to sell that stock.Return the
+   maximum profit you can achieve from this transaction.
+   If you cannot achieve any profit, return 0.
+ */
+int maxProfit(List<int> prices) {
+  int minPrice = prices[0];
+  int maxProfit = 0;
+
+  for (int i = 1; i < prices.length; i++) {
+    // Update minimum price (best buying point)
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    }
+    // Calculate profit if sold today
+    else {
+      int profit = prices[i] - minPrice;
+      if (profit > maxProfit) {
+        maxProfit = profit;
+      }
+    }
+  }
+
+  return maxProfit;
+}
